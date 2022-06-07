bb4142d24bc666d8fa41892331b284b1c0da0b72
hoon <dltjdgnsok1@naver.com>
2022-06-07 10:32:43 +0900

    HTTP API 메시지 바디에 담아서 전송

---
+++ src/main/java/hoon/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hoon.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+public class HelloData {
+    String username;
+    int age;
+}
