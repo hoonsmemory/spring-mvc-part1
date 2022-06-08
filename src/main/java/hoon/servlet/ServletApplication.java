6cc756104f6bf49d975eafd78ff4d90d7d99ff78
hoon <dltjdgnsok1@naver.com>
2022-06-08 21:00:43 +0900

    scanBasePackages 변경

--- src/main/java/hoon/servlet/ServletApplication.java
+++ src/main/java/hoon/servlet/ServletApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan //서블릿 자동 등록
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "hoon")
 public class ServletApplication {
 
 	public static void main(String[] args) {
