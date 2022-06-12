1a481ae7e6c5b77631b6d3deb70a050bf3430cb2
hoon <dltjdgnsok1@naver.com>
2022-06-12 21:01:20 +0900

    상품 등록

---
+++ src/main/java/hoon/springmvc/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,57 @@
+package hoon.springmvc.itemservice.web.basic;
+
+import hoon.springmvc.itemservice.domain.item.Item;
+import hoon.springmvc.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/basic/items")
+@Controller
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 10));
+    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId,
+                       Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute("item") Item item) {
+
+        itemRepository.save(item);
+        return "basic/item";
+    }
+}
